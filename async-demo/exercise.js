75d4c2ca2ed2f9f6307bab50ee198b44a0dfd2e0
Mewan Dimalsha <115539818+mewanDimalsha@users.noreply.github.com>
2024-09-21 00:29:08 +0530

    commit changes

--- async-demo/exercise.js
+++ async-demo/exercise.js
@@ -13,24 +13,23 @@
 //         }
 //     })
 //     .catch(err => console.log('Error', err.message));
-
-    const notifyCustomer = async (id) => {
-        try {
-            const customer = await getCustomer(id);
-            console.log('Customer: ', customer);
-            if (customer.isGold) {
-                const movies = await getTopMovies();
-                console.log('Top movies: ', movies);
-                await sendEmail(customer.email, movies);
-                console.log('Email sent...');
-            }
-        } catch (err) {
-            console.log('Error', err.message);
+const notifyCustomer = async (id) => {
+    try {
+        const customer = await getCustomer(id);
+        console.log('Customer: ', customer);
+        if (customer.isGold) {
+            const movies = await getTopMovies();
+            console.log('Top movies: ', movies);
+            await sendEmail(customer.email, movies);
+            console.log('Email sent...');
         }
-    };
+    } catch (err) {
+        console.log('Error', err.message);
+    }
+};
     
-    // Example usage
-    notifyCustomer(1);
+// Example usage
+notifyCustomer(1);
 
   function getCustomer(id) {
     return new Promise((resolve, reject) => {
