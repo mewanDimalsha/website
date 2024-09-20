9b97bac9614956c660d12152fa805dced3859d81
Mewan Dimalsha <115539818+mewanDimalsha@users.noreply.github.com>
2024-09-21 00:22:12 +0530

    changes

---
+++ async-demo/exercise.js
@@ -0,0 +1,62 @@
+
+// Exercise 1: Convert the following code from a callback hell to a promise chain:
+// getCustomer(1)
+//     .then(customer => {
+//         console.log('Customer: ', customer);
+//         if (customer.isGold) {
+//             getTopMovies()
+//                 .then(movies => {
+//                     console.log('Top movies: ', movies);
+//                     sendEmail(customer.email, movies)
+//                         .then(() => console.log('Email sent...'));
+//                 });
+//         }
+//     })
+//     .catch(err => console.log('Error', err.message));
+
+    const notifyCustomer = async (id) => {
+        try {
+            const customer = await getCustomer(id);
+            console.log('Customer: ', customer);
+            if (customer.isGold) {
+                const movies = await getTopMovies();
+                console.log('Top movies: ', movies);
+                await sendEmail(customer.email, movies);
+                console.log('Email sent...');
+            }
+        } catch (err) {
+            console.log('Error', err.message);
+        }
+    };
+    
+    // Example usage
+    notifyCustomer(1);
+
+  function getCustomer(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ 
+                id: id, 
+                name: 'Mosh Hamedani', 
+                isGold: true, 
+                email: 'email' 
+              });
+        }, 4000);
+    }); 
+  }
+  
+  function getTopMovies() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(['movie1', 'movie2']);
+        }, 4000);
+    });
+  }
+  
+  function sendEmail(email, movies) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 4000);
+    });
+  }
--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,31 @@
 console.log('before');
-const user = getUser(1);
-console.log(user);
+// getUser(1, (user) => {
+//     console.log('User', user);
+//     getRepositories(user.gitHubUsername, (repos) => {
+//         console.log('Repos', repos);
+//     });
+// });
 console.log('after');
 
+getUser(1)
+    .then(user=> getRepositories(user.gitHubUsername))
+    .then(repos => console.log('Repos', repos))
+    .catch(err => console.log('Error', err.message));
+    
+
 function getUser(id) {
-    setTimeout(() => {
-        console.log('Reading a user from a database...');
-        return { id: id, gitHubUsername: 'mosh' };
-    }, 2000);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({id: id, gitHubUsername: 'mosh' });
+        }, 2000);
+    });
 }
+
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });}
